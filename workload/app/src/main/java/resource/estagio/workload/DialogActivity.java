8912a2c4d4bdb4093a87d8a605910b1a2e6a6af0
Guilherme <gmattos505@gmail.com>
2019-05-21 12:48:49 -0300

    Correção de marge modais

--- workload/app/src/main/java/resource/estagio/workload/DialogActivity.java
+++ workload/app/src/main/java/resource/estagio/workload/DialogActivity.java
@@ -1,13 +1,12 @@
 package resource.estagio.workload;
 
 import android.app.Dialog;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class DialogActivity extends AppCompatActivity {
 
