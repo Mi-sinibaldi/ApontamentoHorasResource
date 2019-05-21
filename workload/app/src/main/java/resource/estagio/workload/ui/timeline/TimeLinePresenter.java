d9b7f987e25a52d0acbf5b692b11645b2a3c1404
Guilherme Sartori <guilhermesassarico@gmail.com>
2019-05-21 17:40:22 -0300

    ações dos botões de avançar e voltar

--- workload/app/src/main/java/resource/estagio/workload/ui/timeline/TimeLinePresenter.java
+++ workload/app/src/main/java/resource/estagio/workload/ui/timeline/TimeLinePresenter.java
@@ -21,6 +21,7 @@ public class TimeLinePresenter implements TimeLineContract.Presenter {
     @Override
     public void getTimeline(int month, int year) {
         hours = 0;
+        view.dialog(true);
         EmployeeDomain employeeDomain = new EmployeeDomain();
         employeeDomain.irepository = new EmployeeRepository();
 
@@ -29,11 +30,13 @@ public class TimeLinePresenter implements TimeLineContract.Presenter {
             public void onSuccessful(List<TimeEntryModel> value) {
                 view.showListTimeline(value);
                 view.showSucessMessage("Sucesso");
+                view.dialog(false);
             }
 
             @Override
             public void onUnsuccessful(String error) {
                 view.showErrorMessage("Erro\n"+error);
+                view.dialog(false);
             }
         });
 
