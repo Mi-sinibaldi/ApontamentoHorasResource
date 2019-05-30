ab5f8024ff6d771424fdc2a7ea98608c564a59f3
Catia <catiamsouza80@gmail.com>
2019-05-30 13:56:37 -0300

    Ajustes

--- workload/app/src/main/java/resource/estagio/workload/ui/employee/EmployeePresenter.java
+++ workload/app/src/main/java/resource/estagio/workload/ui/employee/EmployeePresenter.java
@@ -1,8 +1,6 @@
 package resource.estagio.workload.ui.employee;
 
-import androidx.fragment.app.Fragment;
 
-import resource.estagio.workload.data.remote.model.EmployeeModel;
 
 public class EmployeePresenter implements EmployeeContract.Presenter {
 
