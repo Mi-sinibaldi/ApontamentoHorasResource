cb34377bdb61febd7536cc2856057bd500a6729c
Catia <catiamsouza80@gmail.com>
2019-05-30 10:04:50 -0300

    Revert "Merge branch 'telaColaborador' into dev"
    This reverts commit f06df687d0cc317c032fc16d21a38c20428157b3.

--- workload/app/src/main/java/resource/estagio/workload/ui/employee/adapterEmployee/AdapterEmployee.java
+++ workload/app/src/main/java/resource/estagio/workload/ui/employee/adapterEmployee/AdapterEmployee.java
@@ -1,14 +1,15 @@
 package resource.estagio.workload.ui.employee.adapterEmployee;
 
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
